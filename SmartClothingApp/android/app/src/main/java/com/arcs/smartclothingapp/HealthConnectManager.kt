1294085312f339e9f1cc72f7fa3751a3c91cb158
Viper-one-one <taylor.j.nastally@gmail.com>
2023-11-28 10:10:31 -0800

    added basic structure for health connect

---
+++ SmartClothingApp/android/app/src/main/java/com/arcs/smartclothingapp/HealthConnectManager.kt
@@ -0,0 +1,141 @@
+package com.arcs.smartclothingapp;
+
+import android.content.Context
+import android.os.Build
+import androidx.compose.runtime.mutableStateOf
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.aggregate.AggregationResult
+import androidx.health.connect.client.metadata.DataOrigin
+import androidx.health.connect.client.permission.HealthDataRequestPermissions
+import androidx.health.connect.client.permission.Permission
+import androidx.health.connect.client.records.*
+import androidx.health.connect.client.request.AggregateRequest
+import androidx.health.connect.client.request.ReadRecordsRequest
+import androidx.health.connect.client.time.TimeRangeFilter
+import java.time.Instant
+
+// The minimum android level that can use Health Connect
+const val MIN_SUPPORTED_SDK = Build.VERSION_CODES.O_MR1
+
+/**
+ * Demonstrates reading and writing from Health Connect.
+ */
+class HealthConnectManager(private val context: Context) {
+    private val healthConnectClient by lazy { HealthConnectClient.getOrCreate(context) }
+
+    var availability = mutableStateOf(HealthConnectAvailability.NOT_SUPPORTED)
+        private set
+
+    init {
+        availability.value = when {
+            HealthConnectClient.isAvailable(context) -> HealthConnectAvailability.INSTALLED
+            isSupported() -> HealthConnectAvailability.NOT_INSTALLED
+            else -> HealthConnectAvailability.NOT_SUPPORTED
+        }
+    }
+
+    /**
+     * Determines whether all the specified permissions are already granted. It is recommended to
+     * call [PermissionController.getGrantedPermissions] first in the permissions flow, as if the
+     * permissions are already granted then there is no need to request permissions via
+     * [HealthDataRequestPermissions].
+     */
+    suspend fun hasAllPermissions(permissions: Set<Permission>): Boolean {
+        return permissions == healthConnectClient.permissionController.getGrantedPermissions(
+            permissions
+        )
+    }
+
+
+
+    /**
+     * Obtains a list of [ActivitySession]s in a specified time frame. An Activity Session is a
+     * period of time given to an activity, that would make sense to a user, e.g. "Afternoon run"
+     * etc. It does not necessarily mean, however, that the user was *running* for that entire time,
+     * more that conceptually, this was the activity being undertaken.
+     */
+    suspend fun readActivitySessions(start: Instant, end: Instant): List<ActivitySession> {
+        val request = ReadRecordsRequest(
+            recordType = ActivitySession::class,
+            timeRangeFilter = TimeRangeFilter.between(start, end)
+        )
+        val response = healthConnectClient.readRecords(request)
+        return response.records
+    }
+
+
+    suspend fun getDailyStepCountSamples(start: Instant, end: Instant): List<Steps> {
+        val request = ReadRecordsRequest(
+            recordType = Steps::class,
+            timeRangeFilter = TimeRangeFilter.between(start, end)
+        )
+        val response = healthConnectClient.readRecords(request)
+        return response.records
+    }
+
+    suspend fun getDailyHeartRateCount(start: Instant, end: Instant): List<HeartRateSeries> {
+        val request = ReadRecordsRequest(
+            recordType = HeartRateSeries::class,
+            timeRangeFilter = TimeRangeFilter.between(start, end)
+        )
+        val response = healthConnectClient.readRecords(request)
+        return response.records
+    }
+
+    suspend fun getDailySleepSamples(start: Instant, end: Instant): List<SleepSession> {
+        val request = ReadRecordsRequest(
+            recordType = SleepSession::class,
+            timeRangeFilter = TimeRangeFilter.between(start, end)
+        )
+        val response = healthConnectClient.readRecords(request)
+        return response.records
+    }
+
+    suspend fun getAggregatedStepsSamples(start: Instant, end: Instant): Long {
+
+        val timeRangeFilter = TimeRangeFilter.between(
+            startTime = start,
+            endTime = end
+        )
+        val aggregateDataTypes = setOf(Steps.COUNT_TOTAL)
+
+        val aggregateRequest = AggregateRequest(
+            metrics = aggregateDataTypes,
+            timeRangeFilter = timeRangeFilter
+        )
+        val response = healthConnectClient.aggregate(aggregateRequest)
+        val steps = response[Steps.COUNT_TOTAL]?.toLong() ?: 0L
+        return steps
+    }
+
+
+    /**
+     * Convenience function to reuse code for reading data.
+     */
+    private suspend inline fun <reified T : Record> readData(
+        timeRangeFilter: TimeRangeFilter,
+        dataOriginFilter: List<DataOrigin> = listOf()
+    ): List<T> {
+        val request = ReadRecordsRequest(
+            recordType = T::class,
+            dataOriginFilter = dataOriginFilter,
+            timeRangeFilter = timeRangeFilter
+        )
+        return healthConnectClient.readRecords(request).records
+    }
+
+    private fun isSupported() = Build.VERSION.SDK_INT >= MIN_SUPPORTED_SDK
+}
+
+/**
+ * Health Connect requires that the underlying Healthcore APK is installed on the device.
+ * [HealthConnectAvailability] represents whether this APK is indeed installed, whether it is not
+ * installed but supported on the device, or whether the device is not supported (based on Android
+ * version).
+ */
+enum class HealthConnectAvailability {
+    INSTALLED,
+    NOT_INSTALLED,
+    NOT_SUPPORTED
+}
